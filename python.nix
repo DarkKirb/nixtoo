8608cf5e44920598aca0f54ae3c26123721552fc
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-01-11 18:59:14 +0100

    afdko: disable tests

--- python.nix
+++ python.nix
@@ -1,5 +1,8 @@
 # Python package overrides
 self': super': self: super: { 
+    afdko = super.afdko.overrideAttrs {
+        doInstallCheck = false;
+    };
     click = super.click.overrideAttrs {
         doInstallCheck = false; # Broken on ZFS
     };
